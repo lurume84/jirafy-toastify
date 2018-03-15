197d88c7e0c31ebf08f7c579ea0b9b9c10f23628
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-03-15 12:50:46 +0100

    Add 'Thank You' page

--- js/api.js
+++ js/api.js
@@ -1,8 +1,9 @@
+/* GitHub */
 function listTopics(repository, callback)
 {
     $.ajax({
         url: 'https://api.github.com/repos/' + repository + '/topics',
-        type: 'GET',
+        method: 'GET',
         headers: { 'Accept': 'application/vnd.github.mercy-preview+json' }
     }).done(callback);
 }
@@ -10,4 +11,12 @@ function listTopics(repository, callback)
 function getLatestRelease(repository, callback)
 {
     $.get('https://api.github.com/repos/' + repository + '/releases/latest', null, callback);
+}
+
+/* https://forismatic.com/en/api/ */
+function getRandomQuote(key, callback)
+{
+    if (key === undefined || key == null)
+        key = Math.floor(Math.random() * 100000);
+    $.getJSON('https://api.forismatic.com/api/1.0/?lang=en&format=json&method=getQuote&key=' + key, callback);
 }
--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,68 @@ function generateMailTo()
         $(this).replaceWith($('<a href="' + aHref + '">' + aText + '</a>'));
     });
 }
+
+/*==================*/
+/* 'Thank You' Page */
+/*==================*/
+
+var ty_emotes = [
+    0x2764, 0x2764, 0x2764, 0x2764,             // :heart:
+    0x01F497, 0x01F497,                         // :heartpulse:
+    0x01F44D, 0x01F44D,                         // :+1:
+    0x01F62E,                                   // :open_mouth:
+    0x01F632,                                   // :astonished:
+    0x2615, 0x2615                              // :coffee:
+];
+
+function spawnHearts()
+{
+    var heart = document.createElement('span');
+    $(heart).addClass('unselectable ty-heart').hide();
+    $(heart).text(String.fromCodePoint(pickRandom(ty_emotes), 0xFE0F));
+    $('body').append(heart);
+    
+    var left = 100 * Math.random();
+    var xDelta = (Math.random() - 0.5) * 30;
+    var final_left = left + xDelta;
+    var final_bottom = 100 * getRandomArbitrary(0.6, 1);
+    var animDuration = 10000 * getRandomArbitrary(0.5, 1);
+    
+    var animProperties = {
+        left: final_left + 'vw',
+        bottom: final_bottom + 'vh',
+    };
+    var animOptions = {
+        queue: false,
+        duration: animDuration,
+        complete: function() {
+            $(this).remove();
+        }
+    };
+    
+    $(heart).css({ left: left + 'vw' })
+            .animate(animProperties, animOptions)
+            .fadeIn({
+                queue: false,
+                duration: animDuration * getRandomArbitrary(0.2, 0.5)
+            })
+            .delay(animDuration * getRandomArbitrary(0.3, 0.6))
+            .fadeOut(animDuration * getRandomArbitrary(0.1, 0.3));
+    
+    setTimeout(spawnHearts, 1000 * getRandomArbitrary(1, 2));
+}
+
+/*-------*/
+/* MATHS */
+/*-------*/
+
+function getRandomArbitrary(min, max)
+{
+    return Math.random() * (max - min) + min;
+}
+
+function pickRandom(array)
+{
+    var i = Math.floor(Math.random() * array.length);
+    return array[i];
+}
